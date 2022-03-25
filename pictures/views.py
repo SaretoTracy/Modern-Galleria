fc969a5ae423cf27233f708e5988393bf54a15f0
saretotracy <tracysareto@gmail.com>
2022-03-25 14:16:25 +0300

    pass in date

--- pictures/views.py
+++ pictures/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
+import datetime as dt
 
 # Create your views here.
-def welcome(request):
-    return HttpResponse('Welcome to Modern Galleria')
+def welcome(request):    
+    date = dt.date.today()
+    html = f'''
+        <html>
+            <body>
+                <h1> {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
