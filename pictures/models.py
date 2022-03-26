70c42741fdcb842cf01f4dc5e6a02a780acfdbeb
saretotracy <tracysareto@gmail.com>
2022-03-26 22:50:08 +0300

    add a modal class

--- pictures/models.py
+++ pictures/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Galleria(models.Model):
+    name = models.CharField(max_length=60)
+    image_pic = models.ImageField(upload_to = 'pics/', null=True)
+    description = models.TextField()
+    # location = models.ManyToManyField(location)
+    # category = models.ManyToManyField(category)
+    pub_date = models.DateTimeField(auto_now_add=True)
