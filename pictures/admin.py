fc743381eda7547a7f60c562b7f1e18b4f17cb6d
saretotracy <tracysareto@gmail.com>
2022-03-27 09:12:19 +0300

    add a location relationship to Galore class

--- pictures/admin.py
+++ pictures/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Galore,location
+from .models import Galore,Location
 # Register your models here.
 
 admin.site.register(Galore)
-admin.site.register(location)
+admin.site.register(Location)
--- pictures/models.py
+++ pictures/models.py
@@ -1,11 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Location(models.Model):
+    location = models.CharField(max_length =30)
+    def __str__(self):
+        return self.location
+    def save_location(self):
+        self.save()
+    def delete_location(self):
+        self.delete()
 class Galore(models.Model):
     title = models.CharField(max_length=60)
     image_pic = models.ImageField(upload_to = 'photos/', null=True)
     description = models.TextField()
-    # location = models.ManyToManyField(location)
+    location = models.ManyToManyField('location')
     # category = models.ManyToManyField(category)
     pub_date = models.DateTimeField(auto_now_add=True)
 
@@ -17,16 +25,3 @@ class Galore(models.Model):
     def delete_galleria(self):
         self.delete()
 
-    # @classmethod
-    # def days_post(cls,date):
-    #     post = cls.objects.filter(pub_date__date = date)
-    #     return post
-
-class location(models.Model):
-    locate = models.CharField(max_length =30)
-    def __str__(self):
-        return self.locate
-    def save_location(self):
-        self.save()
-    def delete_location(self):
-        self.delete()
